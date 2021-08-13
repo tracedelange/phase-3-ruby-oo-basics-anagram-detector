889e76f893f21d94beab40266a4cdb356aa682aa
Trace <tracedelange@me.com>
2021-08-13 10:14:00 -0500

    commit tree e17c6581ecb1f1abd3d1851bdd035664e1855be5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+class Anagram
+
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(list)
+        res = []
+        list.each do |item|
+            item_chars = item.chars
+            target_chars = @word.chars
+            
+            # puts item_chars.sort
+            # puts target_chars.sort
+
+            if item_chars.sort == target_chars.sort
+                res.push(item)
+            end
+        end
+        return res
+    end
+end
